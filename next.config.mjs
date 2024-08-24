e8c7e741ee4ae0d6c2186bdb1e2ccea980ee5cc3
Antonio Fulgencio <antoniofulg@gmail.com>
2024-08-24 01:39:13 -0300

    Add redirects

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,30 @@ const nextConfig = {
 		locales: ["en-CA", "fr-CA"],
 		defaultLocale: "en-CA",
 	},
+	redirects: async () => {
+		return [
+			{
+				source: "/akshay",
+				destination: "/bathtubs",
+				permanent: true,
+			},
+			{
+				source: "/wojciech",
+				destination: "/fr-CA/douches",
+				permanent: true,
+			},
+			{
+				source: "/lukasz",
+				destination: "/",
+				permanent: true,
+			},
+			{
+				source: "/antonio",
+				destination: "/fr-CA",
+				permanent: true,
+			},
+		]
+	},
 }
 
 export default nextConfig
